543f10ed73a1be29e84f0709efe9c70eca4039ce
Ruslan <tagizaderuslan25@gmail.com>
2024-08-02 20:30:11 +0400

    Inital commit

---
+++ PersonalManagementApp/UIForm/XtraHome.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace UIForm
+{
+    partial class XtraHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(XtraHome));
+            ribbonControl1 = new DevExpress.XtraBars.Ribbon.RibbonControl();
+            btnEmployees = new DevExpress.XtraBars.BarButtonItem();
+            btnClose = new DevExpress.XtraBars.BarButtonItem();
+            barButtonItem2 = new DevExpress.XtraBars.BarButtonItem();
+            barButtonItem3 = new DevExpress.XtraBars.BarButtonItem();
+            ribbonPage1 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            ribbonPageGroup2 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            ((System.ComponentModel.ISupportInitialize)ribbonControl1).BeginInit();
+            SuspendLayout();
+            // 
+            // ribbonControl1
+            // 
+            ribbonControl1.ExpandCollapseItem.Id = 0;
+            ribbonControl1.Items.AddRange(new DevExpress.XtraBars.BarItem[] { ribbonControl1.ExpandCollapseItem, btnEmployees, btnClose, barButtonItem2, barButtonItem3 });
+            ribbonControl1.Location = new Point(0, 0);
+            ribbonControl1.MaxItemId = 5;
+            ribbonControl1.Name = "ribbonControl1";
+            ribbonControl1.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] { ribbonPage1 });
+            ribbonControl1.QuickToolbarItemLinks.Add(barButtonItem2);
+            ribbonControl1.QuickToolbarItemLinks.Add(barButtonItem3);
+            ribbonControl1.Size = new Size(1285, 183);
+            // 
+            // btnEmployees
+            // 
+            btnEmployees.Caption = "Empoyee list";
+            btnEmployees.Id = 1;
+            btnEmployees.ImageOptions.LargeImage = (Image)resources.GetObject("btnEmployees.ImageOptions.LargeImage");
+            btnEmployees.Name = "btnEmployees";
+            // 
+            // btnClose
+            // 
+            btnClose.Caption = "Close";
+            btnClose.Id = 2;
+            btnClose.ImageOptions.LargeImage = (Image)resources.GetObject("btnClose.ImageOptions.LargeImage");
+            btnClose.Name = "btnClose";
+            btnClose.ItemClick += btnClose_ItemClick;
+            // 
+            // barButtonItem2
+            // 
+            barButtonItem2.Caption = "barButtonItem2";
+            barButtonItem2.Id = 3;
+            barButtonItem2.Name = "barButtonItem2";
+            // 
+            // barButtonItem3
+            // 
+            barButtonItem3.Caption = "barButtonItem3";
+            barButtonItem3.Id = 4;
+            barButtonItem3.Name = "barButtonItem3";
+            // 
+            // ribbonPage1
+            // 
+            ribbonPage1.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] { ribbonPageGroup1, ribbonPageGroup2 });
+            ribbonPage1.Name = "ribbonPage1";
+            ribbonPage1.Text = "ribbonPage1";
+            // 
+            // ribbonPageGroup1
+            // 
+            ribbonPageGroup1.ItemLinks.Add(btnEmployees);
+            ribbonPageGroup1.Name = "ribbonPageGroup1";
+            ribbonPageGroup1.Text = "Register";
+            // 
+            // ribbonPageGroup2
+            // 
+            ribbonPageGroup2.ItemLinks.Add(btnClose);
+            ribbonPageGroup2.Name = "ribbonPageGroup2";
+            ribbonPageGroup2.Text = "Close";
+            // 
+            // XtraHome
+            // 
+            AutoScaleDimensions = new SizeF(7F, 16F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1285, 693);
+            Controls.Add(ribbonControl1);
+            Name = "XtraHome";
+            Text = "EmployeeControl";
+            WindowState = FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)ribbonControl1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DevExpress.XtraBars.Ribbon.RibbonControl ribbonControl1;
+        private DevExpress.XtraBars.BarButtonItem btnEmployees;
+        private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage1;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup1;
+        private DevExpress.XtraBars.BarButtonItem btnClose;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem2;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem3;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup2;
+    }
+}
---
+++ PersonalManagementApp/UIForm/XtraHome.cs
@@ -0,0 +1,26 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UIForm
+{
+    public partial class XtraHome : DevExpress.XtraEditors.XtraForm
+    {
+        public XtraHome()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
