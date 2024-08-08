b0a53da7d7e380199a4cc5b34771589266e58e4b
Ruslan <tagizaderuslan25@gmail.com>
2024-08-08 18:24:07 +0400

    Added Data Access Layer and DbContext class

---
+++ PersonalManagementApp/DAL/Repositories/Concrete/EntityFrameworkCore/PMDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories.Concrete.EntityFrameworkCore
+{
+    public class PMDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"server=DESKTOP-JVM62VU\SQLEXPRESS;database=PMDb;Trusted_Connection=true");
+        }
+    }
+}
