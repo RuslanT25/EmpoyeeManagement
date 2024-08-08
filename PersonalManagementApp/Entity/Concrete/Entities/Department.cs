62d78de02ff74085d99eac1125895f524097f21c
Ruslan <tagizaderuslan25@gmail.com>
2024-08-08 18:38:28 +0400

    Added Empoyee and Department entities,BaseEntity,Automapper

---
+++ PersonalManagementApp/Entity/Concrete/Entities/Department.cs
@@ -0,0 +1,15 @@
+﻿using Entity.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Concrete.Entities
+{
+    public class Department : BaseEntity
+    {
+        public string Name { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ PersonalManagementApp/Entity/Concrete/Entities/Employee.cs
@@ -0,0 +1,24 @@
+﻿using Entity.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Concrete.Entities
+{
+    public class Employee : BaseEntity
+    {
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime StartingDate { get; set; }
+        public DateTime? EndingDate { get; set; }
+        public string? ReasonOfLeaving { get; set; }
+        public string IdentityNumber { get; set; }
+        public string Status { get; set; }
+    }
+}
